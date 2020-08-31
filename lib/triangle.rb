fda809bf65ee1bb70fd984c98f7d79fac9b2369d
Yani Asari <yani.iman1@gmail.com>
2020-08-31 06:17:25 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -5,9 +5,8 @@ class Triangle
   @sides = []
   
   def initialize(side1, side2, side3)
-    @side1 = side1
-    @side2 = side2
-    @side3 = side3
+     @side1, @side2, @side3 = side1, side2, side3
+    @sides = [side1, side2, side3]
   end 
   
   def kind
